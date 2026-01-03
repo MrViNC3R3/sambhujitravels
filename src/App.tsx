2faa0d6f1014dd08df82d79225aca12321a25dac
MrViNC3R3 <rogervincere2@gmail.com>
2026-01-03 16:45:02 +0530

    first commita

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
+import About from './pages/About';
+import ServicesPage from './pages/Services';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
